7ce26294b9b12ff2c4e9279e753859ec8148cdfc
Prachi-lal <plalindia01@gmail.com>
2025-04-21 14:21:33 +0530

    calling a third party api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+import requests
 from dotenv import load_dotenv
 import os
 
@@ -6,11 +7,13 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/health', methods=["GET","POST"])
-def health():
-    if request.method=="GET":
-        return jsonify(status="ok",method="GET"),200
-    if request.method=="POST":
-        return jsonify(status="ok",method="POST"),200
-
-
+@app.route('/')
+def call_api():
+    res = requests.get('https://en.wikipedia.org/wiki/Valorant')
+    if res.status_code == 200:
+        return {"message":res.text}
+    elif res.status_code == 404:
+        return {"message":"Something went wrong"}, 404
+    else:
+        return {"message":"Server Error"}, 500
+    
